5dbec612c6efb8c4df344f5030ccf73fb7b6460e
Tom Shannon <tomshannon@email.com>
2013-01-26 21:55:04 -0700

    Update Cron_P21_WO_Import.rb

--- Cron_P21_WO_Import.rb
+++ Cron_P21_WO_Import.rb
@@ -30,7 +30,7 @@ theDispositions = []
 
 woTableDBID = "bfkavuvrv"
 username = "reg.rms@regencylighting.com"
-password = "R3g.Rm5pa55"
+password = "PASSWORD"
 qbc = QuickBase::Client.new(username,password)
 qbc.printRequestsAndResponses = false
 
