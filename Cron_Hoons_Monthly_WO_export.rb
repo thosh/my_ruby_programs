947427616e9014832da72eb7d6ac959506ca9f79
Tom Shannon <tomshannon@email.com>
2013-01-26 21:54:13 -0700

    Update Cron_Hoons_Monthly_WO_export.rb

--- Cron_Hoons_Monthly_WO_export.rb
+++ Cron_Hoons_Monthly_WO_export.rb
@@ -1,7 +1,7 @@
 require 'C:\Ruby\lib\ruby\gems\1.9.1\gems\quickbase_client-1.0.4\lib\QuickBaseClient.rb'
 tableDBID = "bfkavuvrv"
 username = "reg.rms@regencylighting.com"
-password = "R3g.Rm5pa55"
+password = "PASSWORD"
 t = Time.now
 f = "\\\\Regfileshare01\\rms\\Conduit2.0_WOs_Export\\Work_Orders"+ t.strftime("%Y%m") + ".csv"
 qbc = QuickBase::Client.new(username,password)
